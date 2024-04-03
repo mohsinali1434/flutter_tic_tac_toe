0c8e89b6f6c7cb6e517d703ddc578d729cbb53d4
Muhammad Mohsin <3028ma@gmail.com>
2024-04-03 07:56:22 +0500

    first commit

---
+++ lib/game_button.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class GameButton {
+  final id;
+  String text;
+  Color bg;
+  bool enabled;
+
+  GameButton(
+      {this.id,
+      this.text = "",
+      this.bg = Colors.blueAccent,
+      this.enabled = true});
+}
